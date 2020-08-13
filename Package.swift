0bd4b257ee29c20e63fc3ce35148e91acc0640e4
Thomas Kalhøj Clemensen <tkc@trifork.com>
2020-08-13 10:43:57 +0200

    Package manifedt

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TriforkSwiftDependencyInjection",
-            path: "TriforkSwiftDependencyInjection/TriforkSwiftDependencyInjection"
+            path: "TriforkSwiftDependencyInjection/Sources"
             dependencies: []),
         .testTarget(
             name: "TriforkSwiftDependencyInjectionTests",
-            path: "TriforkSwiftDependencyInjectionTests/TriforkSwiftDependencyInjectionTests",
+            path: "TriforkSwiftDependencyInjectionTests/Tests",
             dependencies: ["TriforkSwiftDependencyInjection"]),
     ]
 )
