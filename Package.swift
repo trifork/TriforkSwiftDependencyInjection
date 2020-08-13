7854e5333cf71923081c934e0fe28cbd3bf6565b
Thomas Kalhøj Clemensen <tkc@trifork.com>
2020-08-13 10:45:25 +0200

    You gotta remember that comma

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TriforkSwiftDependencyInjection",
-            path: "TriforkSwiftDependencyInjection/Sources"
+            path: "TriforkSwiftDependencyInjection/Sources",
             dependencies: []),
         .testTarget(
             name: "TriforkSwiftDependencyInjectionTests",
